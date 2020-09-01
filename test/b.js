8c677c3ccd956aaa3c2ef96a310be1da6dc49b2d
liudan185 <liudan185@jd.com>
2020-09-01 10:38:16 +0800

    fix: 删除测试脚本

--- test/b.js
+++
@@ -1,3 +0,0 @@
-const a = require('./a.js');
-console.log('in b, a.a1 = %j, a.a2 = %j', a.a1, a.a2);
-exports.done = 123;
--- test/main.js
+++
@@ -1,2 +0,0 @@
-const a = require('./a');
-console.log('in main, a.a1 = %j, a.a2 = %j', a.a1, a.a2);
