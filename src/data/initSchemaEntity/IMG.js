e00297e2da4b49f31c1b869fe2cb5bf4cdbc2919
liudan185 <liudan185@jd.com>
2020-09-08 17:08:33 +0800

    fix: 图片上传组件去掉imgRatio

--- src/data/initSchemaEntity/IMG.js
+++ src/data/initSchemaEntity/IMG.js
@@ -9,8 +9,6 @@ export const initIMGData = {
   description: '上传图片', // 字段项的说明和描述
   imgWidth: 200, // 图片默认的宽度，单位默认px
   imgHeight: 200, // 图片默认的高度，单位默认px
-  imgRatio: 1, // 图片宽高比例是否可调整
   imgRatioReadOnly: false, // 图片宽高比例是否可调整
   isRequired: false,
-  readOnly: false,
 };
