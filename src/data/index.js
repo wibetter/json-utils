009a9b5d12bc9638935178197a93f845555c56b8
liudan185 <liudan185@jd.com>
2021-04-22 11:50:24 +0800

    feat: 新增下拉单选类型

--- src/data/index.js
+++ src/data/index.js
@@ -6,6 +6,7 @@ export * from './initSchemaEntity/Textarea';
 export * from './initSchemaEntity/TextEditor';
 export * from './initSchemaEntity/Number';
 export * from './initSchemaEntity/Radio';
+export * from './initSchemaEntity/SingleSelect'; // 下拉单选
 export * from './initSchemaEntity/Select';
 
 export * from './initSchemaEntity/DateTime';
