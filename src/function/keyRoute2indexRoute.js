f4b765a0b6be64908e6fe0eaea167bd23e1fdf98
wibetter <betterliu@ghs>
2021-10-17 22:02:10 +0800

    fix: 完善keyRoute2indexRoute方法

--- src/function/keyRoute2indexRoute.js
+++ src/function/keyRoute2indexRoute.js
@@ -24,8 +24,8 @@ export function keyRoute2indexRoute(keyRoute, targetJsonSchemaObj) {
         // 2、根据key值获取对应的json数据对象
         curJsonSchemaObj = curJsonSchemaObj.properties[curKey]; // 对象类型数据引用
       } else if (curJsonSchemaObj.items) {
-        // 兼容数据类型
-        curIndex = curKey;
+        // 兼容数组类型
+        curIndex = 0; // curKey;
         curJsonSchemaObj = curJsonSchemaObj.items; // 对象类型数据引用
       }
       curIndexRoute = curIndexRoute
